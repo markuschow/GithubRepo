7376386fe19216a893cf8d43c4b765e8b7569933
Markus Chow <markuschow@gmail.com>
2020-07-19 22:39:20 +0200

    add Specta

--- Example/Pods/Target Support Files/Pods-GithubRepo_Example/Pods-GithubRepo_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-GithubRepo_Example/Pods-GithubRepo_Example-frameworks.sh
@@ -199,10 +199,12 @@ fi
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/AFNetworking/AFNetworking.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GithubRepo/GithubRepo.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Specta/Specta.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/AFNetworking/AFNetworking.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GithubRepo/GithubRepo.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Specta/Specta.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
